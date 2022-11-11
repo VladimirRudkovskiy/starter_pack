dc3d39ff27cded2af47a7a6411beda83c7d19328
Vladimir <rudkovskyvladimir21051@gmail.com>
2022-11-11 11:16:06 +0200

    starter pack

---
+++ src/components/link.js
@@ -0,0 +1,13 @@
+/** @jsx jsx */
+import { jsx, NavLink as MenuLink, Link as A } from 'theme-ui';
+import NextLink from 'next/link';
+export function NavLink({ path, label, children, ...rest }) {
+  return (
+		<h1>NavLink</h1>
+  );
+}
+export function Link({ path, label, children, ...rest }) {
+  return (
+		<h1>Link</h1>
+  );
+}
